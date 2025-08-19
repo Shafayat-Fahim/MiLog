d1b9a29e2c935ff18608ed6c2149f6cbd150d5ac
Shafayat Fahim <theanonto@outlook.com>
2025-08-20 03:57:27 +0600

    Delete init.sql

--- mysql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS fuel_logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    vehicle_name VARCHAR(100) NOT NULL,
-    odometer_km FLOAT NOT NULL,
-    fuel_liters FLOAT NOT NULL,
-    date_added DATE NOT NULL
-);
